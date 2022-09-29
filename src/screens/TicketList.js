49e5e5b04e70064a338238b498725633c4f8b6d5
jerrywebie <jerry.webie@gmail.com>
2022-09-30 01:49:53 +0630

    setup screens and navigation

---
+++ src/screens/TicketList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function TicketList() {
+  return (
+    <View>
+      <Text>Ticket List</Text>
+    </View>
+  );
+}
